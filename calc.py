d50e4c72eee5eb26037ea2b38c06605cc1a47652
ysun647 <yimingsun1994@gmail.com>
2019-11-24 00:07:38 -0800

    first commit

---
+++ calc.py
@@ -0,0 +1,29 @@
+import sys
+
+# Permutation from y to x.
+# e.g. p(5,3) means 5*4*3.
+def p(x, y):
+	if x <= y:
+		return y
+	return x * p(x-1, y)
+
+# Combination of y from x.
+# e.g. c(5,3) means (5*4*3) / (3*2*1) 
+def c(x, y):
+	m = p(x, x-y+1)
+	n = p(y, 1)
+	return m / n
+
+# Shortcut to get command line arguments
+def g(x):
+	return int(sys.argv[x])
+
+# Usage: python calc.py <x1> <y1> <x2> <y2>
+# Return: c(x1, y1) / c(x2, y2)
+if __name__ == '__main__':
+	assert len(sys.argv) == 5
+	m = c(g(1), g(2))
+	n = c(g(3), g(4))
+
+	print("Possibility: ", m / n)
+	print("One minus Possibility: ", 1 - m / n)
